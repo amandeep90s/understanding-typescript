f3d436c56f5dd9411e0a8bd68945634769fc6dc0
Amandeep Singh <programmeramandeep@gmail.com>
2023-06-05 14:44:27 +0530

    Using types

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Your code goes here!!");
+function add(number1, number2) {
+    return number1 + number2;
+}
+var number1 = 10;
+var number2 = 5.8;
+var result = add(number1, number2);
+console.log(result);
